05870802fc99fd20bbee407ed5dcbbb54484fbc8
Mikhail Khrenov <mkhrenov34@gmail.com>
2018-07-08 18:27:08 -0400

    Revised regex slightly

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < elements.length; i++) {
     let element = elements[i];
 
     let text = element.innerHTML;
-    element.innerHTML = text.replace(/\.e([a-f0-9]{5})\{.*?\}/gi, '.e$1{background: no-repeat url(https://www.gstatic.com/chat/emoji/7h/emoji_u$1.png);height: 28px;background-size: auto 100%;width: 28px;}');
+    element.innerHTML = text.replace(/\.e([a-f0-9]+)\{.*?\}/gi, '.e$1{background: no-repeat url(https://www.gstatic.com/chat/emoji/7h/emoji_u$1.png);height: 28px;background-size: auto 100%;width: 28px;}');
 
 }
 
